218f31ec3bbb7bd0b5b025f7fb597fa224cc0feb
lilinglin <lilinglin@u51.com>
2018-09-08 22:24:13 +0800

    招银

--- src/main/java/笔试/招银/Main.java
+++ src/main/java/笔试/招银/Main.java
@@ -1,5 +1,9 @@
 package 笔试.招银;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * @Author: lilinglin
  * @Description:
@@ -7,62 +11,86 @@ package 笔试.招银;
  */
 public class Main {
 
+//    public static int fun(int x){
+//        if(x==0 || x==1){
+//            return x+1;
+//        }
+//        return fun(x-1)+fun(x-2);
+//    }
+//
+//    public static void test3(){
+//        System.out.println(fun(10));
+//    }
+//
+//    public static void main(String[] args) {
+//        test3();
+//    }
+
     public static void main(String[] args) {
-        sumList(90);
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int n,m;
+        List<Integer> list = new ArrayList();
+        for (int i = 0;i<t;i++){
+            n = sc.nextInt();
+            m = sc.nextInt();
+            list.add(check(n,m));
+        }
+        for (Integer result : list){
+            System.out.println(result);
+        }
     }
 
-    public static void printList(int a, int b) {
-
-        for (int i = a; i <= b; i++) {
-
-            System.out.print(i + " ");
-
+    public static int check(int n,int m) {
+        int result = 0;
+        if ((n - 2) >= 0 && (m - 2) >= 0) {
+            result += (n - 2) * (m - 2);
         }
 
-        System.out.println();
+        result += (m - 2)>0?m-2:0;
+        result += check1(0, 0, n, m);
 
+        if ((n - 1) != 0) {
+            result += (m - 2)>0?m-2:0;
+            result+=check1(n-1,0,n,m);
+        }
+
+        result += (n - 2)>0?n - 2:0;
+        result += check1(0, m-1, n, m);
+
+        if ((m - 1) != 0) {
+            result += (n - 2)>0?n - 2:0;
+            result += check1(n-1, m-1, n, m);
+        }
+            return result;
     }
 
-    public static void sumList(int s) {
-
-        int start = 1;
-
-        int end = 2;
-
-        int half = (s + 1) / 2;
-
-        int sum = start + end;
-
-        while (start < half) {
-
-            if (sum == s) {
-
-                sum = sum - start;
-
-                printList(start, end);
-
-                start++;
-
-                end++;
-
-                sum = sum + end;
-
-            } else if (sum < s) {
-
-                end++;
-
-                sum+=end;
-
+        public static int check1( int a, int b, int n, int m){
+            int count = 1;
+            if (checkIndex(a - 1, b - 1, n, m)) count++;
+            if (checkIndex(a - 1, b, n, m)) count++;
+            if (checkIndex(a - 1, b + 1, n, m)) count++;
+            if (checkIndex(a, b - 1, n, m)) count++;
+            if (checkIndex(a, b + 1, n, m)) count++;
+            if (checkIndex(a + 1, b - 1, n, m)) count++;
+            if (checkIndex(a + 1, b, n, m)) count++;
+            if (checkIndex(a + 1, b + 1, n, m)) count++;
+            if (count % 2 != 0) {
+                return 1;
             } else {
-
-                sum-=start;
-
-                start++;
-
+                return 0;
             }
-
         }
+
+        public static boolean checkIndex ( int x, int y, int n, int m){
+            if (x >= 0 && x <= n - 1 && y >= 0 && y <= m - 1) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+
 
     }
 
-}
